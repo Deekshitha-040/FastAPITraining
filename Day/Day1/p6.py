a6ede4e9f9077687210f24fe0fa9de2bbcecf0ed
Deekshitha-040 <2025cs_deekshithabv_b@nie.ac.in>
2026-09-21 14:58:12 +0530

    dictionary

---
+++ Day/Day1/p6.py
@@ -0,0 +1,5 @@
+#Dictionary: A collection of key-value pairs
+student = {"name": "John", "age": 20, "grade": "A"}
+print(student["name"])  # Output: John
+print(student["age"])   # Output: 20
+print(student["grade"]) # Output: A
