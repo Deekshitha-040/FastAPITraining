5e7eb49147cdf991627c455f212f3bc436c35c1a
Deekshitha-040 <2025cs_deekshithabv_b@nie.ac.in>
2026-09-22 15:19:47 +0530

    pydantic model

--- Day/Day2/FastAPIPractice/main.py
+++ Day/Day2/FastAPIPractice/main.py
@@ -10,4 +10,28 @@ def about():
     return {"page": "About!","author": "Dexter"}
 @app.get("/health")   
 def health():
-    return {"status": "Healthy!"}     
+    return {"status": "Healthy!"}  
+#POST request
+@app.post("/create")
+def create_something():
+    return {"message": "Something created!"}
+#path parameter
+@app.get("/student/{usn}")
+def get_result(usn):
+    return {"Result":"Distinction","USN":usn} 
+      
+#path parameter with type hint
+@app.get("/candidate/{rollno}")
+def get_candidate(rollno: int):
+    return {"Result":"Distinction","Roll No":rollno, "Type": type(rollno)}
+
+#Pydantic model
+from pydantic import BaseModel
+class Item(BaseModel):
+    name: str
+    price: float
+    in_stock: bool=True
+@app.post("/item")
+def create_item(item: Item):
+    return {"received": item, "price": item.price*1.18} 
+       
